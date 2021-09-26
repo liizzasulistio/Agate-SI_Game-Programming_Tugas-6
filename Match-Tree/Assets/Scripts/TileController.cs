4d75a8e95de54510cd689cc585e75dae1dbc3ba1
Lii'zza Sulistio <liizzasulistio@gmail.com>
2021-09-26 20:03:38 +0700

    get to know the neighbors

--- Match-Tree/Assets/Scripts/TileController.cs
+++ Match-Tree/Assets/Scripts/TileController.cs
@@ -7,6 +7,9 @@ public class TileController : MonoBehaviour
     private static readonly Color selectedColor = new Color(0.5f, 0.5f, 0.5f);
     private static readonly Color normalColor = Color.white;
 
+    private static readonly float moveDuration = 0.5f;
+
+    private static readonly Vector2[] adjacentDirection = new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
     private static TileController previousSelected = null;
     private bool isSelected = false;
 
@@ -31,7 +34,7 @@ public class TileController : MonoBehaviour
 
     private void OnMouseDown()
     {
-        if(render.sprite == null)
+        if(render.sprite == null || board.IsAnimating)
         {
             return;
         }
@@ -41,9 +44,12 @@ public class TileController : MonoBehaviour
         }
         else
         {
-            if(previousSelected == null)
+            if(GetAllAdjacentTiles().Contains(previousSelected))
             {
-                Select();
+                TileController otherTile = previousSelected;
+                previousSelected.Deselect();
+
+                SwapTile(otherTile, () => { SwapTile(otherTile); });
             }
             else
             {
@@ -66,5 +72,52 @@ public class TileController : MonoBehaviour
         render.color = normalColor;
         previousSelected = null;
     }
+    #endregion
+
+    #region Swapping & Moving
+    public void SwapTile(TileController otherTile, System.Action onCompleted = null)
+    {
+        //StartCoroutine(board.SwapTilePosition(this, otherTile, onCompleted));
+    }
+
+    public IEnumerator MoveTilePosition(Vector2 targetPosition, System.Action onCompleted)
+    {
+        Vector2 startPos = transform.position;
+        float time = 0.0f;
+
+        yield return new WaitForEndOfFrame();
+        while(time < moveDuration)
+        {
+            transform.position = Vector2.Lerp(startPos, targetPosition, time / moveDuration);
+            time += Time.deltaTime;
+
+            yield return new WaitForEndOfFrame();
+        }
+        transform.position = targetPosition;
+        onCompleted?.Invoke();
+    }
+    #endregion
+
+    #region Adjacent
+    private TileController GetAdjacent(Vector2 castDir)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, castDir, render.size.x);
+        if(hit)
+        {
+            return hit.collider.GetComponent<TileController>();
+        }
+        return null;
+    }
+
+    public List<TileController> GetAllAdjacentTiles()
+    {
+        List<TileController> adjacentTiles = new List<TileController>();
+        for(int i = 0; i < adjacentDirection.Length; i++)
+        {
+            adjacentTiles.Add(GetAdjacent(adjacentDirection[i]));
+        }
+        return adjacentTiles;
+    }
+
     #endregion
 }
