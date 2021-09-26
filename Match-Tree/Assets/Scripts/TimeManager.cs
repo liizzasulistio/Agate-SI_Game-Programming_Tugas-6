c03cf732c13ad7e0b4f4f7bc5d414378796e7e74
Lii'zza Sulistio <liizzasulistio@gmail.com>
2021-09-26 22:40:04 +0700

    still bug...

---
+++ Match-Tree/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public int duration;
+    private float time;
+
+    #region Singleton
+    private static TimeManager _instance = null;
+    public static TimeManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<TimeManager>();
+
+                if (_instance == null)
+                {
+                    Debug.LogError("Fatal Error: TimeManager not Found");
+                }
+            }
+            return _instance;
+        }
+    }
+    #endregion
+
+    private void Start()
+    {
+        time = 0;
+    }
+
+    private void Update()
+    {
+        if (GameFlowManager.Instance.IsGameOver)
+        {
+            return;
+        }
+
+        if (time > duration)
+        {
+            GameFlowManager.Instance.GameOver();
+            return;
+        }
+
+        time += Time.deltaTime;
+    }
+
+    public float GetRemainingTime()
+    {
+        return duration - time;
+    }
+}
