dc01756a0b05d4f6dec2313c493c22253f21131f
Lii'zza Sulistio <liizzasulistio@gmail.com>
2021-09-26 18:38:51 +0700

    add tile prefab and boardmanager

---
+++ Match-Tree/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    #region Singleton
+    private static BoardManager _instance = null;
+
+    public static BoardManager Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType<BoardManager>();
+                if(_instance == null)
+                {
+                    Debug.LogError("Fatal Error: BoardManager not found");
+                }
+            }
+            return _instance;
+        }
+    }
+    #endregion
+}
